1cc3de367396c1ead22258ed0da95549a8dc2cc4
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2023-04-05 22:11:17 +0200

    Addes test

--- src/test/groovy/no/fintlabs/authentication/TokenServiceSpec.groovy
+++ src/test/groovy/no/fintlabs/authentication/TokenServiceSpec.groovy
@@ -3,7 +3,6 @@ package no.fintlabs.authentication
 import no.fintlabs.Props
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
-import org.junit.Ignore
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.test.StepVerifier
@@ -19,7 +18,7 @@ class TokenServiceSpec extends Specification {
         mockWebServer.start()
         tokenService = new TokenService(
                 props,
-                WebClient.create(), metricService
+                WebClient.create(), Mock(MetricService.class)
         )
     }
 
@@ -27,21 +26,21 @@ class TokenServiceSpec extends Specification {
         mockWebServer.shutdown()
     }
 
-//    def "When getting access token a token should be present"() {
-//        given:
-//        mockWebServer.enqueue(new MockResponse()
-//                .addHeader("Content-Type", "application/json")
-//                .setBody('{"access_token": "tokenValue"}'))
-//
-//        when:
-//        def tokenMono = tokenService.fetchToken("localhost:" + mockWebServer.getPort())
-//
-//        then:
-//        StepVerifier
-//                .create(tokenMono)
-//                .expectNextMatches(token -> token.getAccessToken() == "tokenValue")
-//                .verifyComplete()
-//    }
+    def "When getting access token a token should be present"() {
+        given:
+        mockWebServer.enqueue(new MockResponse()
+                .addHeader("Content-Type", "application/json")
+                .setBody('{"access_token": "tokenValue"}'))
+
+        when:
+        def tokenMono = tokenService.fetchToken("localhost:" + mockWebServer.getPort())
+
+        then:
+        StepVerifier
+                .create(tokenMono)
+                .expectNextMatches(token -> token.getAccessToken() == "tokenValue")
+                .verifyComplete()
+    }
 
 //    def "If we don't get a valid response from the IDP an exception should be thrown"() {
 //        when:
