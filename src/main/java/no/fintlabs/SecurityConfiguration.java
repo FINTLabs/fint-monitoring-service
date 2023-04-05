f7691f21000c5aaa867687d22966a1f9ead6c7ea
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2023-04-05 10:28:43 +0200

    Disabled ssl verification

--- src/main/java/no/fintlabs/SecurityConfiguration.java
+++ src/main/java/no/fintlabs/SecurityConfiguration.java
@@ -1,10 +1,17 @@
 package no.fintlabs;
 
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import javax.net.ssl.SSLException;
 
 @Configuration
 public class SecurityConfiguration {
@@ -19,53 +26,18 @@ public class SecurityConfiguration {
                 .httpBasic().disable()
                 .build();
     }
-//
-//    @Bean
-//    public Authentication dummyAuthentication() {
-//        return new UsernamePasswordAuthenticationToken("drosje", "loyve", Collections.emptyList());
-//    }
-//
-//    @Bean
-//    public ReactiveOAuth2AuthorizedClientManager authorizedClientManager(ReactiveClientRegistrationRepository clientRegistrationRepository,
-//                                                                         ReactiveOAuth2AuthorizedClientService authorizedClientService) {
-//
-//        ReactiveOAuth2AuthorizedClientProvider authorizedClientProvider = ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
-//                .password().refreshToken().build();
-//
-//        AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager authorizedClientManager =
-//                new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientService);
-//
-//        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
-//
-//        authorizedClientManager.setContextAttributesMapper(contextAttributesMapper());
-//
-//        return authorizedClientManager;
-//    }
-//
-//    private Function<OAuth2AuthorizeRequest, Mono<Map<String, Object>>> contextAttributesMapper() {
-//        return authorizeRequest -> {
-//            Map<String, Object> contextAttributes = new HashMap<>();
-//            contextAttributes.put(OAuth2AuthorizationContext.USERNAME_ATTRIBUTE_NAME, authorizeRequest.getAttribute(OAuth2ParameterNames.USERNAME));
-//            contextAttributes.put(OAuth2AuthorizationContext.PASSWORD_ATTRIBUTE_NAME, authorizeRequest.getAttribute(OAuth2ParameterNames.PASSWORD));
-//            return Mono.just(contextAttributes);
-//        };
-//    }
-
-//    @Bean
-//    public ClientHttpConnector clientHttpConnector() {
-//        HttpClient httpClient = HttpClient.create(ConnectionProvider.builder("laidback").maxLifeTime(Duration.ofHours(1)).maxIdleTime(Duration.ofMinutes(1)).build())
-//                .tcpConfiguration(tcpClient -> tcpClient.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 300000)
-//                        .doOnConnected(connection -> connection
-//                                .addHandlerLast(new ReadTimeoutHandler(120))
-//                                .addHandlerLast(new WriteTimeoutHandler(120))));
-//
-//        return new ReactorClientHttpConnector(httpClient);
-//    }
 
     @Bean
-    public WebClient webClient(WebClient.Builder builder) {
+    public WebClient webClient(WebClient.Builder builder) throws SSLException {
+
+        SslContext sslContext = SslContextBuilder
+                .forClient()
+                .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                .build();
+        HttpClient httpClient = HttpClient.create().secure(t -> t.sslContext(sslContext));
 
         return builder
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .defaultHeader("Host", "idp.felleskomponent.no")
                 .build();
     }
